56dcc05ed5711ec0787a98d243a9a58f8bc9eb52
Nathanna <hello@nathannaofficial.com>
2021-03-16 18:00:43 -0400

    fix

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const killp = require('kill-port');
 killp(80, "tcp");
-killp(3000, "tcp");
+killp(3000, "tcp").then(function() {
+    const control = require("./control.js");
+    const api = require("./api.js");
+    api.setProp(control);
 
-const api = require("./api.js");
-const control = require("./control.js");
-
-api.setProp(control);
-
-let id = control.make(25565, "spigot", 1);
+    let id = control.make(25565, "spigot", 1);
+});
